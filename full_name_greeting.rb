a7d27bf688afb42eff615b696f095f5ea5253e64
EC2 Default User <ec2-user@ip-172-31-38-103.us-east-2.compute.internal>
2019-08-24 09:48:55 +0000

    Initial commit

---
+++ full_name_greeting.rb
@@ -0,0 +1,9 @@
+puts 'Hello! What is your first name?
+first_name = gets.chomp
+puts 'Great! Do you have a middle name. If so, what is it?'
+middle_name = gets.chomp
+puts 'And what is your surname?'
+surname = gets.chomp
+puts 'So your full name is ' first_name + '.' + middle_name + '.' + surname + '!'
+
+
