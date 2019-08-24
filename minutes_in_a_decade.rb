5869531433773ede3c1c840f8aea77dde3cbf22b
EC2 Default User <ec2-user@ip-172-31-38-103.us-east-2.compute.internal>
2019-08-24 09:36:40 +0000

    Initial commit

---
+++ minutes_in_a_decade.rb
@@ -0,0 +1,7 @@
+#Minutes in a decade
+#60 mins in an hour
+#24 hours in a day
+#365 days in a year
+#10 years in a decade
+#To calculate the number of minutes in a decade = 60 * 24 * 365 * 10
+puts 60 * 24 * 365 * 10
