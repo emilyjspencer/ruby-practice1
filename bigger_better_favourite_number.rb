3f1e2cc941971d57e32d9e38eff205a9a475814d
Emily Spencer <emilyspencer89@hotmail.co.uk>
2019-08-24 11:51:19 +0000

    Initial commit

---
+++ bigger_better_favourite_number.rb
@@ -0,0 +1,6 @@
+#Bigger, better, favourite number
+puts 'What is your favourite number? '
+favourite_num = gets.chomp.to_i
+favourite_num += 1
+
+puts 'That\'s fine, but what about ' + favourite_num.to_s + ' ? '
