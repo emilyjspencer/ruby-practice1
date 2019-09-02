b2bf6fb2279d1a4a5ad4baca9cbe3538afa8f2ca
Emily Spencer <emilyspencer89@hotmail.co.uk>
2019-09-02 12:34:15 +0000

    Playing with loops and using one line if statements

---
+++ one_line_if_statement.rb
@@ -0,0 +1,10 @@
+# Playing with while loops and using one line if statements
+
+# Print all even numbers less than 100
+
+number = 0
+
+while true do
+number += 1
+  puts number if number < 100 && number.even?
+end
