97a923176a19947cbee6ba1a70b6ca59d3faec95
Emily Spencer <emilyspencer89@hotmail.co.uk>
2019-09-05 12:51:43 +0000

    German - English dictionary

---
+++ german_english_dictionary.rb
@@ -0,0 +1,13 @@
+german_english_dictionary = 
+{
+"Hallo" => "Hello",
+"Bis bald" => "See you soon",
+"Danke schoen" => "Thank you very much",
+"Ich bin krank" => "I am ill",
+"Du bist gluecklich" => "You are happy",
+"eine Katze" => "a cat",
+"lernen" => "to learn"
+}
+
+# To output the English for "Du bist gluecklich":
+puts german_english_dictionary["Du bist gluecklich"]
