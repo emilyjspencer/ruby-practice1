0eff8857126d9aa9c438d284e60d24b10b368427
Emily Spencer <emilyspencer89@hotmail.co.uk>
2019-09-06 15:19:50 +0000

    FizzBuzz in Ruby

--- fizzbuzz_ruby.rb
+++ fizzbuzz_ruby.rb
@@ -1,17 +1,17 @@
 #For the numbers 1 to 20 inclusive- numbers divisible by 3 should be replaced by 'Fizz', numbers divisible by 5 should be
 #replaced by 'Buzz', numbers divisible by both 3 and 5 should be replaced by 'FizzBuzz'.The rest of the numbers should simply be printed.
-  (1..20).each do |number|
-    if number % 3 == 0 && number % 5 == 0
+  (1..20).each { |num|
+    if num % 3 == 0 && num % 5 == 0
       puts 'FizzBuzz'
-    elsif number % 3 == 0
+    elsif num % 3 == 0
       puts 'Fizz'
-    elsif number % 5 == 0
+    elsif num % 5 == 0
       puts 'Buzz'
     else
-      puts number
+      puts num
     end
-   end
-  end
+  }
+  
 
 
 
