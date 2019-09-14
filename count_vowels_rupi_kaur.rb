3281e1f68878ed45e24fdf95f3a298ccbd70bd22
Emily Spencer <emilyspencer89@hotmail.co.uk>
2019-09-14 15:02:35 +0000

    Count vowels in a Rupi Kaur poem

---
+++ count_vowels_rupi_kaur.rb
@@ -0,0 +1,8 @@
+# Rupi Kaur poem - Success
+
+
+string = "of course i want to be successful but i don't crave success for me i need to be successful to gain
+enough milk and honey to help those around me succeed"
+
+vowels = string.downcase.count 'aeiou'
+puts vowels 
